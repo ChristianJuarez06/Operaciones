6b7ca678f001227691a5f4eab98287df88b74081
ChristianJuarez06 <christianjuarez2702@gmail.com>
2024-08-30 11:35:18 -0600

    Crear version 1

---
+++ operaciones.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+  int n1,n2,summa,resta;
+
+  printf("ingresa el primer numero");
+  scanf("%i",&n1);
+  printf("Ingresa el segundo numero");
+  scanf("%i",&n2);
+  printf("Ingresa 1 para hacer suma o ingresa 2 para hacer resta");
+  switch(ops)
+    {
+      case 1:
+        suma=n1+n2;
+        printf("El resultado de la suma es: %i",suma);
+      break;
+      case 2:
+        resta=n1-n2;
+        printf("El resultado de la resta es: %i",resta);
+      break;
+      default:
+        printf("ERROR opcion incorresta debe ser 1 o 2");
+    }
+  return 0; 
+}
