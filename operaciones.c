eb6453132c6d2d824e2dadbf0eede38d52ca2a69
joha318 <joanadelgado318@gmail.com>
2024-09-01 13:09:32 -0600

    Update operaciones.c

--- operaciones.c
+++ operaciones.c
@@ -8,27 +8,31 @@ int main()
   scanf("%f",&n1);
   printf("Ingresa el segundo numero");
   scanf("%f",&n2);
-  printf("Ingresa 1 para hacer suma o ingresa 2 para hacer resta");
+  printf("Ingresa 1 para hacer suma");
+  printf("Ingresa 2 para hacer resta");
+  printf("Ingresa 3 para hacer multiplicación");
+  printf("Ingresa 4 para hacer división");
   scanf("%i",&ops);
+  
   switch(ops)
     {
       case 1:
         suma=n1+n2;
-        printf("El resultado de la suma es: %i",suma);
+        printf("El resultado de la suma es: %.2f",suma);
       break;
       case 2:
         resta=n1-n2;
-        printf("El resultado de la resta es: %i",resta);
+        printf("El resultado de la resta es: %.2f",resta);
       break;
       case 3:
         multiplicacion=n1*n2;
-        printf("El resultado de la multiplicacion es: %i",multiplicacion);
+        printf("El resultado de la multiplicación es: %.2f",multiplicacion);
       break;
       case 4:
         if(n2=!0)
         {
           division=n1/n2;
-        printf("El resultado de la division es: %i",division);
+        printf("El resultado de la división es: %.2f",division);
         }
         else
         {
