6b5b2021f96c6fa4cc5be4a0b0f22947479eaee9
joha318 <joanadelgado318@gmail.com>
2024-09-01 12:49:56 -0600

    Update operaciones.c

--- operaciones.c
+++ operaciones.c
@@ -1,25 +1,45 @@
 #include<stdio.h>
 int main()
 {
-  int n1,n2,summa,resta;
+  int suma,resta,mult,div;
+  float n1, n2;
 
   printf("ingresa el primer numero");
-  scanf("%i",&n1);
+  scanf("%f",&n1);
   printf("Ingresa el segundo numero");
-  scanf("%i",&n2);
-  printf("Ingresa 1 para hacer suma o ingresa 2 para hacer resta");
+  scanf("%f",&n2);
+  printf("Ingresa 1 para hacer suma");
+  printf("Ingresa 2 para hacer resta");
+  printf("Ingresa 3 para hacer multiplicacion");
+  printf("Ingresa 4 para hacer divicion");
+  
   switch(ops)
     {
       case 1:
         suma=n1+n2;
-        printf("El resultado de la suma es: %i",suma);
+        printf("El resultado de la suma es: %.2f",suma);
       break;
       case 2:
         resta=n1-n2;
-        printf("El resultado de la resta es: %i",resta);
+        printf("El resultado de la resta es: %.2f",resta);
+      break;
+      case 3:
+      mult=n1+n2;
+      printf("El resultado de la multiplicacion es de: %.2f",mult);
+      break;
+      case 4:
+      if(n2=!0)
+      {
+        div=n1/n2;
+        printf("El resultado de la divicion es de: %.2f",div);
+      }
+      else 
+      {
+        printf("ERROR en segundo numero debe ser diferente a 0");
+      }
       break;
       default:
-        printf("ERROR opcion incorresta debe ser 1 o 2");
+        printf("ERROR opcion incorresta debe ser 1-4");
     }
   return 0; 
 }
